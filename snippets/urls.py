834a770f1323256bc7bf2c81480167dca09f9104
Nargiza Nassibullina <nssbllnnr>
2020-04-25 18:00:36 +0600

    history-user-acces

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from snippets import views
 
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
-    path('history', views.history_list),
+    path('api/v1/snippets/', views.snippet_list),
+    path('api/v1/snippets/<int:pk>/', views.snippet_detail),
+    path('api/v1/history', views.history_list),
     path('account/create/', views.EmployeesCreateViewSet.as_view()),
     path('list/', views.EmployeesListViewSet.as_view()),
     path('account/detail/<int:pk>/', views.EmployeesDetailViewSet.as_view()),
